26b51d38fc9f57fda076fd89b1801a37d70febb3
yuyun1531 <yuyun1531@163.com>
2024-04-25 00:49:12 +0000

    test

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class VoiceInputChatPage extends StatelessWidget {
           Expanded(
             child: ListView(
               children: [
-                ChatBubble(text: '你好，这是一个语音输入的聊天页面', isUserA: true),
+                ChatBubble(text: '你好，这是一个语音输入的聊天test', isUserA: true),
                 ChatBubble(text: '你好，是的，只能通过语音输入', isUserA: false),
               ],
             ),
